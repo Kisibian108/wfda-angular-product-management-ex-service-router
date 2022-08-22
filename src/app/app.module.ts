50fa40514f2b51d4ff375a85a4967ea3e14c756e
Kisibian108 <102302311+Kisibian108@users.noreply.github.com>
2022-08-22 11:19:46 +0700

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ProductListComponent } from './product/product-list/product-list.component';
-import { ProductCreateComponent } from './product/product-create/product-create.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ProductListComponent} from './product/product-list/product-list.component';
+import {ProductCreateComponent} from './product/product-create/product-create.component';
 import {ReactiveFormsModule} from '@angular/forms';
-import { ProductEditComponent } from './product/product-edit/product-edit.component';
+import {ProductEditComponent} from './product/product-edit/product-edit.component';
 import {ProductDeleteComponent} from './product/product-delete/product-delete.component';
+import {HttpClientModule} from '@angular/common/http';
+import { CategoryListComponent } from './category/category-list/category-list.component';
+import { CategoryEditComponent } from './category/category-edit/category-edit.component';
+import { CategoryCreateComponent } from './category/category-create/category-create.component';
+import { CategoryDeleteComponent } from './category/category-delete/category-delete.component';
 
 @NgModule({
   declarations: [
@@ -15,14 +20,20 @@ import {ProductDeleteComponent} from './product/product-delete/product-delete.co
     ProductListComponent,
     ProductCreateComponent,
     ProductEditComponent,
-    ProductDeleteComponent
+    ProductDeleteComponent,
+    CategoryListComponent,
+    CategoryEditComponent,
+    CategoryCreateComponent,
+    CategoryDeleteComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        ReactiveFormsModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
